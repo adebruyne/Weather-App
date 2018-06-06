ae9fb6ab11d1a89425a84b6e0001289511fbaf38
Aylin DeBruyne <adebruyne88@gmail.com>
2018-06-06 19:43:21 -0400

    added input value

--- weather.js
+++ weather.js
@@ -1,5 +1,38 @@
-function getWeather(location){
-    var latitude = location.lat;
-    var longitude = location.lng;
+$(document).ready(function() {
     
-}
+   $("form").submit(function(event) {
+      event.preventDefault();
+      //zipcode
+      var zipCode = $("#input").val();
+      console.log(zipCode);
+      //Weather API-URL
+  //     var URL = "http://api.openweathermap.org/data/2.5/weather?zip=" + zipCode + ",us&APPID=865f583ab7e2e42228c051145a844358";
+  
+      
+  //    $.get(URL)
+  
+  //     //If successful, do this with the data
+    
+        
+
+
+
+
+  //       // var logo = data.weather[0].icon;
+  //       // var city = data.name;
+  //       // var temperature = Math.round((data.main.temp) * 9/5 - 459.67) + "°";
+  //       // var description = data.weather[0].description;
+  //       // var wind = Math.round(data.wind.speed);
+  //       // $("#logo").attr("src", "http://openweathermap.org/img/w/" + logo + ".png");
+  //       // $("#city").html(city);
+  //       // $("#temperature").html(temperature);
+  //       // $("#description").html(description);
+  //       // $("#wind").html(wind);
+  //     }
+  
+      
+      
+  //  //end form submit
+  
+   });
+  }) 
